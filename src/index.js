13f8daffeb22a8e9da896de7a2f677b98e2c0d9c
saneq1 <saneq90@mail.ru>
2019-03-24 22:26:51 +0300

    addFile

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var a = Number.MAX_VALUE;
+  var j = base;
+
+  for (let i = 2; i <= base; i++) {
+
+ if (j % i === 0) {
+ var count = 0;
+        while (j % i === 0) {
+         j = j / i;     
+         count++;
+      }
+      var c = 0;
+      var z = Math.floor(number / i);
+      while (z > 0) {
+        c += z;
+        z = Math.floor(z / i);
+      }
+      a = Math.min(a, Math.floor(c / count))
+    }
+  }
+  return a;
 }
